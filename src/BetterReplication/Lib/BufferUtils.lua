a8952b0a28b2df038ae78907d5c478d16fe0b067
DevBauke <baukelim@hotmail.nl>
2025-02-15 17:14:54 +0100

    Update to V6.1

--- src/BetterReplication/Lib/BufferUtils.lua
+++ src/BetterReplication/Lib/BufferUtils.lua
@@ -1,5 +1,3 @@
-local BufferUtils = {}
-
 type from_client_packet = {
 	t: number,
 	c: CFrame
@@ -14,32 +12,58 @@ type register_identifier = {
 	id: number
 }
 
+local function readi16AsFloat(b: buffer, cursor: number): number
+	local i16 = buffer.readi16(b, cursor)
+	return i16/1000
+end
+
+local function writeFloatAsi16(b: buffer, cursor: number, value: number)
+	local toWrite = value * 1000
+	buffer.writei16(b, cursor, toWrite)
+end
+
+local function writei24(buff: buffer, offset: number, value: number): () -- thx Mephi!
+	if value < 0 then
+		value = value + 16777216
+	end
+	buffer.writeu16(buff, offset, value / 256)
+	buffer.writeu8(buff, offset + 2, value % 256)
+end
+
+local function readi24(buff: buffer, offset: number): number -- thx Mephi!
+	local value: number = 256 * buffer.readu16(buff, offset) + buffer.readu8(buff, offset + 2)
+	if value >= 8388608 then
+		return value - 16777216
+	end
+	return value
+end
+
 local function writeCFrame(b: buffer, cframe: CFrame): buffer
 	local oldBlen = buffer.len(b)
 	
-	local newB = buffer.create(oldBlen + 24)
+	local newB = buffer.create(oldBlen + 15)
 	buffer.copy(b, 0, newB)
 	
 	local x, y, z = cframe.X, cframe.Y, cframe.Z
 	local rx, ry, rz = cframe:ToEulerAnglesYXZ()
-
-	buffer.writef32(newB, oldBlen + 0, x)
-	buffer.writef32(newB, oldBlen + 4, y)
-	buffer.writef32(newB, oldBlen + 8, z)
-	buffer.writef32(newB, oldBlen + 12, rx)
-	buffer.writef32(newB, oldBlen + 16, ry)
-	buffer.writef32(newB, oldBlen + 20, rz)
+	
+	writei24(b, oldBlen + 0, x*100)
+	writei24(b, oldBlen + 3, y*100)
+	writei24(b, oldBlen + 6, z*100)
+	writeFloatAsi16(b, oldBlen + 9, rx)
+	writeFloatAsi16(b, oldBlen + 11, ry)
+	writeFloatAsi16(b, oldBlen + 13, rz)
 	
 	return newB
 end
 
 local function readCFrame(b: buffer, cursor: number): CFrame
-	local x = buffer.readf32(b, cursor + 0)
-	local y = buffer.readf32(b, cursor + 4)
-	local z = buffer.readf32(b, cursor + 8)
-	local rx = buffer.readf32(b, cursor + 12)
-	local ry = buffer.readf32(b, cursor + 16)
-	local rz = buffer.readf32(b, cursor + 20)
+	local x = readi24(b, cursor + 0)/100
+	local y = readi24(b, cursor + 3)/100
+	local z = readi24(b, cursor + 6)/100
+	local rx = readi16AsFloat(b, cursor + 9)
+	local ry = readi16AsFloat(b, cursor + 11)
+	local rz = readi16AsFloat(b, cursor + 13)
 
 	return CFrame.new(x, y, z) * CFrame.Angles(rx, ry, rz)
 end
@@ -47,25 +71,26 @@ end
 local function writeSimpleCFrame(b: buffer, cframe: CFrame): buffer
 	local oldBlen = buffer.len(b)
 	
-	local newB = buffer.create(oldBlen + 16)
+	local newB = buffer.create(oldBlen + 11)
 	buffer.copy(newB, 0, b)
 
 	local x, y, z = cframe.X, cframe.Y, cframe.Z
 	local rx, ry, rz = cframe:ToEulerAnglesYXZ()
-
-	buffer.writef32(newB, oldBlen + 0, x)
-	buffer.writef32(newB, oldBlen + 4, y)
-	buffer.writef32(newB, oldBlen + 8, z)
-	buffer.writef32(newB, oldBlen + 12, ry)
+	
+	writei24(newB, oldBlen + 0, x*100)
+	writei24(newB, oldBlen + 3, y*100)
+	writei24(newB, oldBlen + 6, z*100)
+	writeFloatAsi16(newB, oldBlen + 9, ry)
 	
 	return newB
 end
 
 local function readSimpleCFrame(b: buffer, cursor: number): CFrame
-	local x = buffer.readf32(b, cursor + 0)
-	local y = buffer.readf32(b, cursor + 4)
-	local z = buffer.readf32(b, cursor + 8)
-	local ry = buffer.readf32(b, cursor + 12)
+	local x = readi24(b, cursor + 0)/100
+	local y = readi24(b, cursor + 3)/100
+	local z = readi24(b, cursor + 6)/100
+	
+	local ry = readi16AsFloat(b, cursor + 9)
 
 	return CFrame.new(x, y, z) * CFrame.Angles(0, ry, 0)
 end
